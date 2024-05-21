375a0f528e42ea119a546d1c120ef3b12b414ba3
Thomas Herstad <thomasherstad@gmail.com>
2024-05-21 09:25:25 +0200

    Added question card behavior.

--- Code/Frontend/static/game.js
+++ Code/Frontend/static/game.js
@@ -1,3 +1,22 @@
 function log(id) {
     console.log(id);
   }; 
+
+// document.getElementsByClassName("question-tile").addEventListener("click", toggle_question);
+
+// function toggle_question(){
+// document.getElementById("question-card").style.display = "show"
+// };
+
+const element = document.getElementById("0,0");
+
+element.addEventListener("click", show_question_card);
+
+function show_question_card() {
+  document.getElementById("question-card").style.display = "block";
+}
+
+//FOR LATER
+function hide_question_card() {
+  document.getElementById("question-card").style.display = "none";
+}
